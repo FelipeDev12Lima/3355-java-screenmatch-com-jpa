161a1ac119aff59299312fc65d5594c826b67424
FelipeDev12Lima <felipe.lima.dev12@gmail.com>
2025-04-09 21:16:39 -0300

    feito o loop para buscar varias series, e também a lógico de listagem das series buscadas

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -21,16 +21,23 @@ public class Principal {
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=6585022c";
 
+    private List<DadosSerie> dadosSeries = new ArrayList<>();
+
     public void exibeMenu() {
+        var opcao = -1;
+        while (opcao != 0) {
+            
+        
         var menu = """
                 1 - Buscar séries
                 2 - Buscar episódios
+                3 - Listar séries buscadas
                 
                 0 - Sair                                 
                 """;
 
         System.out.println(menu);
-        var opcao = leitura.nextInt();
+        opcao = leitura.nextInt();
         leitura.nextLine();
 
         switch (opcao) {
@@ -40,6 +47,9 @@ public class Principal {
             case 2:
                 buscarEpisodioPorSerie();
                 break;
+            case 3:
+                listarSeriesBuscadas();
+                break;
             case 0:
                 System.out.println("Saindo...");
                 break;
@@ -47,9 +57,11 @@ public class Principal {
                 System.out.println("Opção inválida");
         }
     }
+    }
 
     private void buscarSerieWeb() {
         DadosSerie dados = getDadosSerie();
+        dadosSeries.add(dados);
         System.out.println(dados);
     }
 
@@ -71,5 +83,9 @@ public class Principal {
             temporadas.add(dadosTemporada);
         }
         temporadas.forEach(System.out::println);
+    }
+
+    private void listarSeriesBuscadas() {
+        dadosSeries.forEach(System.out::println);
     }
 }
